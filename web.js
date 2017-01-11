a999d3def0ac9fbabad0149c6a9d50200f9218a3
Paul <paul@niobium.me>
2017-01-11 21:28:27 +0100

    Some web.js stuff

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
-var express = require("express");
+  var express = require("express");
 var app = express();
 
 var bot;
 
-var port  = process.env.PORT  || process.env.OPENSHIFT_NODEJS_PORT  || 3000,
+var port  = process.env.PORT  || process.env.OPENSHIFT_NODEJS_PORT  || 8080,
     ip    = process.env.IP    || process.env.OPENSHIFT_NODEJS_IP    || "0.0.0.0"
 
 module.exports = {
   app: app,
   connect: function(bo) {
     app.listen(port, ip);
-    console.log("Web server ready!");
+    console.log("Web server ready! Listening on " + ip + ":" + port);
     bot = bo;
   },
   loadRoute: function(name) {
