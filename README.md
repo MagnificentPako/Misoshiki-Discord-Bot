b7a5a731cc0c261d7fd2d186d5d4338750cd19b1
Paul <paul@niobium.me>
2017-01-13 21:24:27 +0100

    Fixed web.js

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # How to run this crap
 * Create a .env file with your bot secret
 * call ``npm install``
-* call ``node index.js``
+* call ``npm start``
 * ??
 * Profit!
+
+Also, just redirect DisCCord to your new bot URL to get a "private" discord client
--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express();
 
 var bot;
 
-var port  = process.env.PORT  || process.env.OPENSHIFT_NODEJS_PORT  || 3000,
-    ip    = process.env.IP    || process.env.OPENSHIFT_NODEJS_IP    || "localhost"
+var port  = process.env.PORT  || process.env.OPENSHIFT_NODEJS_PORT  || 8080,
+    ip    = process.env.IP    || process.env.OPENSHIFT_NODEJS_IP    || "0.0.0.0"
 
 app.use(bodyParser.json());
 
