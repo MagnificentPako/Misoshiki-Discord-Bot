a487f1131b09b675800a3d3836bd0795e64ac200
Paul <paul@niobium.me>
2017-01-13 21:38:20 +0100

    More stuff

--- web.js
+++ web.js
@@ -8,6 +8,7 @@ var bot;
 var port  = process.env.PORT  || process.env.OPENSHIFT_NODEJS_PORT  || 8080,
     ip    = process.env.IP    || process.env.OPENSHIFT_NODEJS_IP    || "0.0.0.0"
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 module.exports = {
